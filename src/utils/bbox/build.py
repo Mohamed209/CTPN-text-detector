50269db03a8be1a08ee6a7577ddb27b34b0c11ee
mohamed209 <mohamedmosad209@gmail.com>
2020-06-23 19:43:59 +0200

    build script

--- src/utils/bbox/build.py
+++ src/utils/bbox/build.py
@@ -1,7 +1,7 @@
-import subprocess
+import requests
 files = ['nms.pyx', 'bbox.pyx', 'make.sh']
 for file in files:
-    subprocess.run(
-        'wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/Mohamed209/CTPN-text-detector/master/src/utils/bbox/'+file)
-subprocess.run('chmod +x make.sh')
-subprocess.run('bash make.sh')
+    res = requests.get(
+        url='https://raw.githubusercontent.com/Mohamed209/CTPN-text-detector/master/src/utils/bbox/'+file)
+    with open(file, mode='w') as code:
+        code.writelines(res.text)
